76c1574c0e8ba08b7ea01fcb205467a17840ae20
Bender <duspensky@ya.ru>
2012-06-05 09:46:13 +0400

    mlog jni build mibor fix

--- mlog/jni/Android.mk
+++ mlog/jni/Android.mk
@@ -8,7 +8,7 @@ LOCAL_CFLAGS := -DBOOST_SYSTEM_NO_DEPRECATED -DMLOG_USE_BUFFERS=0 -D__GLIBC__
 
 LOCAL_CPP_FEATURES := exceptions rtti
 
-LOCAL_C_INCLUDES := ../../boost ..
+LOCAL_C_INCLUDES := $(BOOST_ROOT) ..
 
 LOCAL_SRC_FILES := ../Logger.cpp      ../Manager.cpp     ../Output.cpp      ../ThreadTrace.cpp ../Utils.cpp
 
