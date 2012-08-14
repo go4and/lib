7888b0171bd9f0aa76c071358e514d1347deda6a
Dmitry Uspenskiy <dusp@asdco.ru>
2012-08-14 20:42:26 +0400

    minimal mcrypt for android

---
+++ mcrypt/jni/Android.mk
@@ -0,0 +1,15 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := mcrypt
+
+LOCAL_CFLAGS := -DBOOST_SYSTEM_NO_DEPRECATED -DMSTD_USE_PBUFFER=0 -D__GLIBC__
+
+LOCAL_CPP_FEATURES := exceptions rtti
+
+LOCAL_C_INCLUDES := $(BOOST_ROOT) $(OPENSSL_ROOT)/include ..
+
+LOCAL_SRC_FILES := ../base64.cpp
+
+include $(BUILD_STATIC_LIBRARY)
