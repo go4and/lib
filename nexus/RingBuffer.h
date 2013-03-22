718c77fce1b40375dc676224d1a879bbf055b5b6
go4and <go4and@gmail.com>
2013-03-22 17:36:36 +0400

    spinlock & strand spinlock

---
+++ nexus/RingBuffer.h
@@ -0,0 +1,190 @@
+#pragma once
+
+namespace nexus {
+
+template<class Buffer, class Op>
+inline typename Op::result_type operation(std::vector<char> & buffer, size_t begin, size_t end, const Op & op, size_t & len)
+{
+    BOOST_ASSERT(begin != end);
+
+    size_t bufferSize = buffer.size();
+    if(end > begin)
+    {
+        len = end - begin;
+        return op(boost::asio::buffer(&buffer[begin], end - begin));
+    } else if(end)
+    {
+        len = (bufferSize - begin) + end;
+        boost::array<Buffer, 0x02> buffers = {
+            Buffer(&buffer[begin], bufferSize - begin),
+            Buffer(&buffer[0], end)
+        };
+        return op(buffers);
+    } else {
+        len = bufferSize - begin;
+        return op(boost::asio::buffer(&buffer[begin], bufferSize - begin));
+    }
+}
+
+template<class Position = volatile size_t>
+class RingBuffer {
+public:
+    typedef Position position_type;
+
+    RingBuffer(size_t size)
+        : bufferSize_(size), buffer_(size)
+    {
+        buffer_.resize(buffer_.capacity());
+        bufferSize_ = buffer_.size();
+    }
+
+    void reset()
+    {
+        writePos_ = 0;
+        readPos_ = 0;
+    }
+
+    inline size_t trim(size_t idx) const
+    {
+        return idx >= bufferSize_ ? idx - bufferSize_ : idx;
+    }
+
+    inline bool full() const
+    {
+        return trim(writePos_ + 1) == readPos_;
+    }
+
+    inline bool empty() const
+    {
+        return writePos_ == readPos_;
+    }
+
+    inline void moveRead(size_t transferred)
+    {
+        readPos_ = trim(readPos_ + transferred);
+    }
+
+    inline void moveWrite(size_t transferred)
+    {
+        writePos_ = trim(writePos_ + transferred);
+    }
+    
+    template<class Op>
+    inline typename Op::result_type read(const Op & op, size_t & len)
+    {
+        return operation<boost::asio::const_buffer>(buffer_, readPos_, writePos_, op, len);
+    }
+    
+    template<class Op>
+    inline typename Op::result_type write(const Op & op, size_t & len)
+    {
+        return operation<boost::asio::mutable_buffer>(buffer_, writePos_, trim(readPos_ + bufferSize_ - 1), op, len);
+    }
+private:
+    std::vector<char> buffer_;
+    size_t bufferSize_;
+    position_type writePos_;
+    position_type readPos_;
+};
+
+template<class Handler>
+class AsyncReadSome {
+public:
+    typedef void result_type;
+
+    AsyncReadSome(boost::asio::ip::tcp::socket & socket, const Handler & handler)
+        : socket_(socket), handler_(handler)
+    {
+    }
+
+    template<class Buffers>
+    void operator()(const Buffers & buffers) const
+    {
+        socket_.async_read_some(buffers, handler_);
+    }
+private:
+    boost::asio::ip::tcp::socket & socket_;
+    const Handler & handler_;
+};
+
+template<class Handler>
+AsyncReadSome<Handler> asyncReadSome(boost::asio::ip::tcp::socket & socket, const Handler & handler)
+{
+    return AsyncReadSome<Handler>(socket, handler);
+}
+
+class ReadSome {
+public:
+    typedef size_t result_type;
+
+    ReadSome(boost::asio::ip::tcp::socket & socket, boost::system::error_code & ec)
+        : socket_(socket), ec_(ec)
+    {
+    }
+
+    template<class Buffers>
+    result_type operator()(const Buffers & buffers) const
+    {
+        return socket_.receive(buffers, 0, ec_);
+    }
+private:
+    boost::asio::ip::tcp::socket & socket_;
+    boost::system::error_code & ec_;
+};
+
+inline ReadSome readSome(boost::asio::ip::tcp::socket & socket, boost::system::error_code & ec)
+{
+    return ReadSome(socket, ec);
+}
+
+template<class Handler>
+class AsyncWriteSome {
+public:
+    typedef void result_type;
+
+    AsyncWriteSome(boost::asio::ip::tcp::socket & socket, const Handler & handler)
+        : socket_(socket), handler_(handler)
+    {
+    }
+
+    template<class Buffers>
+    void operator()(const Buffers & buffers) const
+    {
+        socket_.async_write_some(buffers, handler_);
+    }
+private:
+    boost::asio::ip::tcp::socket & socket_;
+    const Handler & handler_;
+};
+
+template<class Handler>
+AsyncWriteSome<Handler> asyncWriteSome(boost::asio::ip::tcp::socket & socket, const Handler & handler)
+{
+    return AsyncWriteSome<Handler>(socket, handler);
+}
+
+class WriteSome {
+public:
+    typedef size_t result_type;
+
+    WriteSome(boost::asio::ip::tcp::socket & socket, boost::system::error_code & ec)
+        : socket_(socket), ec_(ec)
+    {
+    }
+
+    template<class Buffers>
+    result_type operator()(const Buffers & buffers) const
+    {
+        return socket_.write_some(buffers, ec_);
+    }
+private:
+    boost::asio::ip::tcp::socket & socket_;
+    boost::system::error_code & ec_;
+};
+
+inline WriteSome writeSome(boost::asio::ip::tcp::socket & socket, boost::system::error_code & ec)
+{
+    return WriteSome(socket, ec);
+}
+
+}
---
+++ nexus/Strand.h
@@ -0,0 +1,164 @@
+#pragma once
+
+#ifndef NEXUS_BUILDING
+#include <boost/asio/io_service.hpp>
+
+#include <mstd/spinlock.hpp>
+#endif
+
+namespace nexus {
+
+template<class Mutex = mstd::spinlock>
+class Strand : boost::noncopyable, public boost::asio::detail::operation {
+private:
+    class OnDispatchExit;
+    class StrandRef;
+public:
+    explicit Strand(boost::asio::io_service & ios)
+        : boost::asio::detail::operation(&Strand::doComplete),
+          ios_(boost::asio::use_service<boost::asio::io_service::impl_type>(ios))
+    {
+    }
+
+    template<class Handler>
+    void dispatch(BOOST_ASIO_MOVE_ARG(Handler) handler)
+    {
+        if (boost::asio::detail::call_stack<Strand>::contains(this))
+        {
+            boost::asio::detail::fenced_block b(boost::asio::detail::fenced_block::full);
+            boost_asio_handler_invoke_helpers::invoke(handler, handler);
+            return;
+        }
+
+        typedef boost::asio::detail::completion_handler<Handler> op;
+        typename op::ptr p = { boost::addressof(handler), boost_asio_handler_alloc_helpers::allocate(sizeof(op), handler), 0 };
+        p.p = new (p.v) op(handler);
+
+        bool dispatchImmediately = doDispatch(p.p);
+        boost::asio::detail::operation* o = p.p;
+        p.v = p.p = 0;
+
+        if(dispatchImmediately)
+        {
+            typename boost::asio::detail::call_stack<Strand>::context ctx(this);
+
+            OnDispatchExit onExit(*this);
+            boost::asio::detail::completion_handler<Handler>::do_complete(&ios_, o, boost::system::error_code(), 0);
+        }
+    }
+    
+    template <typename Handler>
+    boost::asio::detail::wrapped_handler<StrandRef, Handler> wrap(Handler handler)
+    {
+        return boost::asio::detail::wrapped_handler<StrandRef, Handler>(StrandRef(*this), handler);
+    }
+private:
+    bool doDispatch(boost::asio::detail::operation* op)
+    {
+        bool can_dispatch = ios_.can_dispatch();
+        mutex_.lock();
+        if (can_dispatch && !locked_)
+        {
+            locked_ = true;
+            mutex_.unlock();
+            return true;
+        }
+
+        if (locked_)
+        {
+            waiting_queue_.push(op);
+            mutex_.unlock();
+        } else {
+            locked_ = true;
+            mutex_.unlock();
+            ready_queue_.push(op);
+            ios_.post_immediate_completion(this);
+        }
+
+        return false;
+    }
+
+    static void doComplete(boost::asio::io_service::impl_type * owner, boost::asio::detail::operation* base, const boost::system::error_code & ec, std::size_t bytes_transferred)
+    {
+        if (owner)
+        {
+            Strand * strand = static_cast<Strand*>(base);
+
+            typename boost::asio::detail::call_stack<Strand>::context ctx(strand);
+
+            OnDoCompleteExit onExit(*strand);
+
+            while(boost::asio::detail::operation* o = strand->ready_queue_.front())
+            {
+                strand->ready_queue_.pop();
+                o->complete(*owner, ec, 0);
+            }
+        }
+    }
+
+    class OnDispatchExit {
+    public:
+        explicit OnDispatchExit(Strand<Mutex> & strand)
+            : strand_(strand)
+        {
+        }
+
+        ~OnDispatchExit()
+        {
+            strand_.mutex_.lock();
+            strand_.ready_queue_.push(strand_.waiting_queue_);
+            bool more_handlers = strand_.locked_ = !strand_.ready_queue_.empty();
+            strand_.mutex_.unlock();
+
+            if (more_handlers)
+                strand_.ios_.post_immediate_completion(&strand_);
+        }
+    private:
+        Strand<Mutex> & strand_;
+    };
+
+    class OnDoCompleteExit {
+    public:
+        explicit OnDoCompleteExit(Strand<Mutex> & strand)
+            : strand_(strand)
+        {
+        }
+
+        ~OnDoCompleteExit()
+        {
+            strand_.mutex_.lock();
+            strand_.ready_queue_.push(strand_.waiting_queue_);
+            bool more_handlers = strand_.locked_ = !strand_.ready_queue_.empty();
+            strand_.mutex_.unlock();
+
+            if (more_handlers)
+                strand_.ios_.post_private_immediate_completion(&strand_);
+        }
+    private:
+        Strand<Mutex> & strand_;
+    };
+    
+    class StrandRef {
+    public:
+        explicit StrandRef(Strand & strand)
+            : strand_(strand)
+        {
+        }
+
+        template<class Handler>
+        void dispatch(BOOST_ASIO_MOVE_ARG(Handler) handler)
+        {
+            strand_.dispatch(BOOST_ASIO_MOVE_CAST(Handler)(handler));
+        }
+    private:
+        Strand & strand_;
+    };
+
+    boost::asio::io_service::impl_type & ios_;
+    Mutex mutex_;
+    bool locked_;
+    boost::asio::detail::op_queue<boost::asio::detail::operation> waiting_queue_;
+    boost::asio::detail::op_queue<boost::asio::detail::operation> ready_queue_;
+};
+
+}
