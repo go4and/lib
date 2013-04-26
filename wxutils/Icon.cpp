c3dbbc3fa55b726875f405a5055a7f29ac49056d
Mahtan <admin@zranger.net>
2013-04-26 22:35:20 +0400

    windows icon features

--- wxutils/Icon.cpp
+++ wxutils/Icon.cpp
@@ -8,13 +8,22 @@ namespace wxutils {
 #if BOOST_WINDOWS
 wxIcon makeIcon(unsigned int resourceId, int sx, int sy)
 {
+    if(sy == -1)
+        sy = sx;
     HICON hIcon = static_cast<HICON>(::LoadImage(instance(), MAKEINTRESOURCE(resourceId), IMAGE_ICON, sx, sy, 0));
     wxIcon icon;
     icon.SetHICON(hIcon);
+    BOOST_ASSERT(icon.IsOk());
     icon.SetSize(wxSize(sx, sy));
     
     return icon;
 }
+
+WXUTILS_DECL void addIcon(wxIconBundle & icons, unsigned int resourceId, int sx, int sy)
+{
+    icons.AddIcon(makeIcon(resourceId, sx, sy));
+}
+
 #endif
 
 }
--- wxutils/Icon.h
+++ wxutils/Icon.h
@@ -6,6 +6,7 @@ class wxIcon;
 
 namespace wxutils {
 
-WXUTILS_DECL wxIcon makeIcon(unsigned int resourceId, int sx, int sy);
+WXUTILS_DECL wxIcon makeIcon(unsigned int resourceId, int sx, int sy = -1);
+WXUTILS_DECL void addIcon(wxIconBundle & icons, unsigned int resourceId, int sx, int sy = -1);
 
 }
